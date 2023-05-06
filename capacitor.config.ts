5439e8ed8e11737b324aa05d57943aa1beae3e8c
Rayk Jackisch <rayk@jackisch.net>
2023-05-06 12:12:27 +0200

    initial

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'appwrite-capacitor-oauth2-test',
-  webDir: 'dist',
+  appId: "io.ionic.starter",
+  appName: "appwrite-capacitor-oauth2-test",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
+    androidScheme: "https",
+    hostname: "dev.fire-and-forget.de"
   }
 };
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -9,48 +9,91 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large"></ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <pre>{{ windowObject }}</pre>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            {{ message }}
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            user.$id:<b>{{ user?.$id ?? "not logged in" }}</b>
+            <br />
+            user.email <b>{{ user?.email ?? "not logged in" }}</b>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <ion-button v-on:click="createEmailSession" expand="block">Login with Email/Password</ion-button>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <ion-button v-on:click="createOAuth2Session" expand="block">Login with Github</ion-button>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <ion-button v-on:click="logout" expand="block">Logout</ion-button>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonButton } from "@ionic/vue";
+import { Client, Account, Models } from "appwrite";
+import { onMounted, ref } from "vue";
+
+const user = ref<Models.User<Models.Preferences>>();
+const account = ref<Account>();
+const message = ref("");
+const windowObject = ref(window.location.origin);
+
+const client = new Client().setEndpoint("https://cloud.fire-and-forget.de/v1").setProject("test");
+account.value = new Account(client);
+
+const createEmailSession = async () => {
+  try {
+    await account.value?.createEmailSession("test@test.dev", "test@test.dev");
+    user.value = await account.value?.get();
+  } catch (error: any) {
+    console.log(error.message);
+    message.value = error.message;
+  }
+};
+
+const createOAuth2Session = () => {
+  account.value?.createOAuth2Session("github", window.location.origin, window.location.origin);
+};
+
+const logout = async () => {
+  try {
+    await account.value?.deleteSessions();
+  } catch (error: any) {
+    console.log(error.message);
+    message.value = error.message;
+  }
+
+  window.location.reload();
+};
+
+onMounted(async () => {
+  try {
+    user.value = await account.value?.get();
+  } catch (error: any) {
+    console.log(error.message);
+    message.value = error.message;
+  }
+});
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
