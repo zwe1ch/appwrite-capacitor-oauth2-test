b7f6cc907d9545504b03b5a6064dbec32e242e17
Rayk Jackisch <rayk@jackisch.net>
2023-05-06 10:32:22 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appwrite-capacitor-oauth2-test',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
